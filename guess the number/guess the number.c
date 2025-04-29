63921c5be33fe55404ebc94118ae4d88b63d44fb
Manaswinipottenolla <manaswinipottenolla@gmail.com>
2025-04-29 23:28:11 +0530

    added a guess the number

---
+++ guess the number/guess the number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int number, guess, attempts = 0;
+    srand(time(0));
+    number = rand() % 100 + 1; // Random number between 1 and 100
+
+    printf("Welcome to the Guess the Number Game!\n");
+    printf("I'm thinking of a number between 1 and 100.\n");
+
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if (guess > number) {
+            printf("Too high!\n");
+        } else if (guess < number) {
+            printf("Too low!\n");
+        } else {
+            printf("Congratulations! You guessed it in %d attempts.\n", attempts);
+        }
+    } while (guess != number);
+
+    return 0;
+}
