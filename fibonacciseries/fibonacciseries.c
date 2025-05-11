b04e9f61b86f992b325cca4c92be3ae4eafd4497
Manaswinipottenolla <manaswinipottenolla@gmail.com>
2025-05-11 07:27:20 +0530

    added a fibonacciseries

--- fibonacciseries/fibonacciseries.c
+++ fibonacciseries/fibonacciseries.c
@@ -1,21 +1,20 @@
-# include<stdio.h>
-int main()
-{
-    int n,first=0,second=1,next;
-    printf("Enter the number of terms:");
-    scanf("%d",&n);
-    printf("Fibonacci serice:");
+#include <stdio.h>
 
-    for(int  i=0;i<n;i++)
-    {
-        if(i<=1)
-        next=i;
-        else{
-            next=first+second;
-            first=second;
-            second=next;
-        }
-        printf("%d",next);
-    }
-    return 0;
+void fibonacci(int n) {
+ int a = 0, b = 1, next;
+ for (int i = 0; i < n; i++) {
+     printf("%d ", a);
+     next = a + b;
+     a = b;
+     b = next;
+ }
+ printf("\n");
+}
+
+int main() {
+ int terms;
+ printf("Enter the number of terms: ");
+ scanf("%d", &terms);
+ fibonacci(terms);
+ return 0;
 }
